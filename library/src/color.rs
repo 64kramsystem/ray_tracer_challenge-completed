c08f8984e93b1c394d405e727874f7b6000bb553
Saverio Miroddi <saverio.pub2@gmail.com>
2020-10-26 16:39:02 +0100

    Chapter 2: Color operations

---
+++ library/src/color.rs
@@ -0,0 +1,70 @@
+use std::ops::{Add, Mul, Sub};
+
+pub const EPSILON: f64 = 1e-6;
+
+#[derive(Debug)]
+pub struct Color {
+    pub r: f64,
+    pub g: f64,
+    pub b: f64,
+}
+
+impl PartialEq for Color {
+    fn eq(&self, rhs: &Self) -> bool {
+        ((self.r - rhs.r).abs() < EPSILON)
+            && ((self.g - rhs.g).abs() < EPSILON)
+            && ((self.b - rhs.b).abs() < EPSILON)
+    }
+}
+
+impl Add for Color {
+    type Output = Color;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Color {
+            r: self.r + rhs.r,
+            g: self.g + rhs.g,
+            b: self.b + rhs.b,
+        }
+    }
+}
+
+impl Sub for Color {
+    type Output = Color;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Color {
+            r: self.r - rhs.r,
+            g: self.g - rhs.g,
+            b: self.b - rhs.b,
+        }
+    }
+}
+
+// The text doesn't specify the range and signedness of the scalar, so a conservative choice is made.
+//
+impl Mul<i32> for Color {
+    type Output = Color;
+
+    fn mul(self, rhs: i32) -> Self::Output {
+        Color {
+            r: self.r * rhs as f64,
+            g: self.g * rhs as f64,
+            b: self.b * rhs as f64,
+        }
+    }
+}
+
+// "Hadamard product".
+//
+impl Mul<Color> for Color {
+    type Output = Color;
+
+    fn mul(self, rhs: Color) -> Self::Output {
+        Color {
+            r: self.r * rhs.r,
+            g: self.g * rhs.g,
+            b: self.b * rhs.b,
+        }
+    }
+}
--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,9 +1,14 @@
 #[macro_use]
 extern crate assert_float_eq;
 
+mod color;
 mod tuple;
 
+pub use color::Color;
 pub use tuple::Tuple;
 
 #[cfg(test)]
 mod tuple_test;
+
+#[cfg(test)]
+mod color_test;
