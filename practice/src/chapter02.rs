76ea347cb61f8c8686b49eae768e67d41091846f
Saverio Miroddi <saverio.pub2@gmail.com>
2020-10-28 19:04:43 +0100

    Chapter 2: Practice

--- practice/src/chapter02.rs
+++ practice/src/chapter02.rs
@@ -1,29 +1,28 @@
-use library::{Color, Sdl2Interface};
+use library::{Color, Sdl2Interface, Tuple};
+
+use crate::chapter01::{Environment, Projectile};
 
 pub fn practice() {
-    let (width, height) = (3, 3);
+    let start = Tuple::point(0, 1, 0);
+    let velocity = Tuple::vector(1, 1.8, 0).normalize() * 11.25;
+    let projectile = Projectile::with_values(start, velocity);
 
-    let mut interface = Sdl2Interface::init("Yay!", width, height);
+    let projectile_color = Color::new(1, 0, 0);
+    let mut interface = Sdl2Interface::init("Chapter 02 exercise", 900, 550);
 
-    for y in 0..height {
-        for x in 0..width {
-            let base_color = match x % 3 {
-                0 => (0.25, 0.0, 0.0),
-                1 => (0.0, 0.25, 0.0),
-                2 => (0.0, 0.0, 0.25),
-                _ => unreachable!(),
-            };
+    let mut environment = Environment::new(projectile);
 
-            // Don't start with (0, 0, 0) - black, which is ugly.
-            //
-            let multiplied_color = Color {
-                r: (base_color.0 * (y + 1) as f64) % 1.0, // won't be 1.0
-                g: (base_color.1 * (y + 1) as f64) % 1.0, // won't be 1.0
-                b: (base_color.2 * (y + 1) as f64) % 1.0, // won't be 1.0
-            };
+    while environment.projectile.position.y > 0.0 {
+        // Watch out the y coordinate conversion!
+        let x_position = environment.projectile.position.x as i16;
+        let y_position =
+            interface.canvas_height() as i16 - environment.projectile.position.y as i16 - 1;
 
-            interface.write_pixel(x as i16, y as i16, multiplied_color);
-        }
+        interface.write_pixel(x_position, y_position, projectile_color);
+
+        println!("Position: x:{}, y:{}", x_position, y_position);
+
+        environment.tick();
     }
 
     interface.update_canvas();
