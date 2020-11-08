f5bf6f02d000d06bb605a7a9201bc856624d32be
Saverio Miroddi <saverio.pub2@gmail.com>
2020-11-08 16:13:27 +0100

    Add transformation concept to Sphere

--- library/src/sphere.rs
+++ library/src/sphere.rs
@@ -1,20 +1,33 @@
 use std::sync::Mutex;
 
+use crate::Matrix;
+
 lazy_static::lazy_static! {
   static ref NEXT_ID: Mutex<u32> = Mutex::new(1);
 }
 
 pub struct Sphere {
     pub id: u32,
+    // Defaults to an identity matrix of order 4.
+    pub transformation: Matrix,
 }
 
 impl Sphere {
-    pub fn new() -> Sphere {
+    pub fn new(transformation: Option<Matrix>) -> Self {
         let mut next_id_mtx = NEXT_ID.lock().unwrap();
 
         let next_id = *next_id_mtx;
         *next_id_mtx += 1;
 
-        Sphere { id: next_id }
+        let transformation = if let Some(transformation) = transformation {
+            transformation
+        } else {
+            Matrix::identity(4)
+        };
+
+        Self {
+            id: next_id,
+            transformation,
+        }
     }
 }
--- library/src/sphere_test.rs
+++ library/src/sphere_test.rs
@@ -2,12 +2,24 @@ use demonstrate::demonstrate;
 
 demonstrate! {
     describe "Sphere" {
+        use crate::matrix::Matrix;
         use crate::sphere::Sphere;
 
+        it "should have an identity transformation of order 4 as default" {
+            assert_eq!(Sphere::new(None).transformation, Matrix::identity(4));
+        }
+
+        it "should allow a transformation to be set" {
+            let expected_transformation = Matrix::scaling(1, 2, 3);
+
+            assert_eq!(Sphere::new(Some(Matrix::scaling(1, 2, 3))).transformation, expected_transformation);
+        }
+
         it "should return consecutive ids for each new Sphere" {
-            assert_eq!(Sphere::new().id, 1);
-            assert_eq!(Sphere::new().id, 2);
-            assert_eq!(Sphere::new().id, 3);
+            let start_id = Sphere::new(None).id;
+
+            assert_eq!(Sphere::new(None).id, start_id + 1);
+            assert_eq!(Sphere::new(None).id, start_id + 2);
         }
     }
 }
