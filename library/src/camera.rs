aeb7c11a87f0ffbf1726b29863d67f74d9279694
Saverio Miroddi <saverio.pub2@gmail.com>
2020-11-16 22:25:43 +0100

    Parallelize Camera#render()

--- library/src/camera.rs
+++ library/src/camera.rs
@@ -1,4 +1,8 @@
-use crate::{Image, Matrix, Ray, Tuple, World};
+use std::sync::Mutex;
+
+use crate::{Color, Image, Matrix, Ray, Tuple, World};
+
+use rayon::prelude::*;
 
 pub struct Camera {
     pub hsize: u16,
@@ -66,14 +70,25 @@ impl Camera {
     // Updates the Image before returning it.
     //
     pub fn render<T: Image>(&self, world: &World) -> T {
-        let mut image = T::new(self.hsize, self.vsize);
+        let mut pixels_buffer =
+            vec![vec![Color::new(0, 0, 0); self.hsize as usize]; self.vsize as usize];
+        let pixels_buffer_mtx = Mutex::new(&mut pixels_buffer);
 
-        for y in 0..self.vsize {
+        (0..self.vsize).into_par_iter().for_each(|y| {
             for x in 0..self.hsize {
                 let ray = self.ray_for_pixel(x, y);
                 let color = world.color_at(&ray);
 
-                image.write_pixel(x as i16, y as i16, color)
+                let mut pixels_buffer = pixels_buffer_mtx.lock().unwrap();
+                pixels_buffer[y as usize][x as usize] = color;
+            }
+        });
+
+        let mut image = T::new(self.hsize, self.vsize);
+
+        for (y, row) in pixels_buffer.iter().enumerate() {
+            for (x, pixel_color) in row.iter().enumerate() {
+                image.write_pixel(x as i16, y as i16, *pixel_color);
             }
         }
 
