d663f411a9597c81f754799567b7398f6c10ae18
Saverio Miroddi <saverio.pub2@gmail.com>
2020-11-21 15:22:08 +0100

    Create `utils` module

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -3,7 +3,6 @@ mod has_float64_value;
 mod image;
 mod material;
 pub mod math;
-mod ppm_encoder;
 mod sdl2_interface;
 pub mod space;
 mod virtual_image;
@@ -11,7 +10,6 @@ mod virtual_image;
 pub use color::Color;
 pub use image::Image;
 pub use material::Material;
-pub use ppm_encoder::PpmEncoder;
 pub use sdl2_interface::Sdl2Interface;
 pub use virtual_image::VirtualImage;
 
@@ -30,9 +28,6 @@ extern crate assert_float_eq;
 
 #[cfg(test)]
 mod color_test;
-
-#[cfg(test)]
-mod ppm_encoder_test;
 
 #[cfg(test)]
 mod material_test;
