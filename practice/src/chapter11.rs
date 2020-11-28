319f92604bc1107200a868499959a436834c66fe
Saverio Miroddi <saverio.pub2@gmail.com>
2020-11-28 22:20:47 +0100

    Chapter 11 practice: Reorganize to make easier to quickly modify

--- practice/src/chapter11.rs
+++ practice/src/chapter11.rs
@@ -12,14 +12,60 @@ use rand::Rng;
 
 const SCREEN_WIDTH: u16 = 400; // height is half
 
+const LIGHT_POSITION: (i32, i32, i32) = (-10, 10, -10);
 const PATTERN_INDEX: Option<u32> = None; // Some(n: ring, checkers+stripe, gradient, stripe, flat), or None for random
 const PATTERN_SCALE: f64 = 0.33;
 const ROTATE_SPHERES: bool = true;
-const REFLECTIVE_CHANCE: f64 = 0.33;
 
-const MIDDLE_SPHERE_COORDS: (f64, f64, f64) = (-0.5, 1.0, 0.5);
-const RIGHT_SPHERE_COORDS: (f64, f64, f64) = (1.5, 0.5, -0.5);
-const LEFT_SPHERE_COORDS: (f64, f64, f64) = (-1.5, 0.33, -0.75);
+#[rustfmt::skip]
+fn add_objects(objects: &mut Vec<Box<dyn Shape>>) {
+    let left_sphere = Sphere {
+        transform: Matrix::translation(-1.5, 0.33, -0.75) * &Matrix::scaling(0.33, 0.33, 0.33) * &random_rotation(),
+        material: random_material(false),
+        ..Sphere::default()
+    };
+
+    let middle_sphere = Sphere {
+        transform: Matrix::translation(-0.5, 1.0, 0.5) * &random_rotation(),
+        material: random_material(true),
+        ..Sphere::default()
+    };
+
+    let right_sphere = Sphere {
+        transform: Matrix::translation(1.5, 0.5, -0.5) * &Matrix::scaling(0.5, 0.5, 0.5) * &random_rotation(),
+        material: random_material(false),
+        ..Sphere::default()
+    };
+
+    let left_wall = Plane {
+        transform: Matrix::translation(0, 0, 5) * &Matrix::rotation(Axis::Y, -PI / 4.0) * &Matrix::rotation(Axis::X, -PI / 2.0),
+        material: random_material(false),
+        ..Plane::default()
+    };
+
+    let floor = Plane {
+        material: random_material(false),
+        ..Plane::default()
+    };
+
+    let right_wall = Plane {
+        transform: Matrix::translation(0, 0, 5) * &Matrix::rotation(Axis::Y, PI / 4.0) * &Matrix::rotation(Axis::X, -PI / 2.0),
+        material: random_material(false),
+        ..Plane::default()
+    };
+
+    objects.push(Box::new(left_sphere));
+    objects.push(Box::new(middle_sphere));
+    objects.push(Box::new(right_sphere));
+
+    objects.push(Box::new(left_wall));
+    objects.push(Box::new(floor));
+    objects.push(Box::new(right_wall));
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// RAND-O-MICE-RS
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn random_color() -> Color {
     Color {
@@ -29,12 +75,8 @@ fn random_color() -> Color {
     }
 }
 
-// If reflective is true, the pattern is always flat, otherwise, it gets ugly.
-//
-fn random_pattern(reflective: bool) -> Box<dyn Pattern> {
-    let pattern_index = if reflective {
-        4
-    } else if let Some(pattern_index) = PATTERN_INDEX {
+fn random_pattern() -> Box<dyn Pattern> {
+    let pattern_index = if let Some(pattern_index) = PATTERN_INDEX {
         pattern_index
     } else {
         rand::thread_rng().gen_range(0, 5)
@@ -89,102 +131,37 @@ fn random_rotation() -> Matrix {
     }
 }
 
-fn random_sphere_material() -> Material {
-    let reflective: bool = rand::random::<f64>() <= REFLECTIVE_CHANCE;
+fn random_material(reflective: bool) -> Material {
+    let pattern = if reflective {
+        Box::new(FlatPattern {
+            color: random_color(),
+            ..FlatPattern::default()
+        })
+    } else {
+        random_pattern()
+    };
+
+    let reflective = reflective as u32 as f64;
 
     Material {
-        pattern: random_pattern(reflective),
+        pattern: pattern,
         diffuse: 0.7,
-        specular: 0.3,
-        reflective: (reflective as u32) as f64,
+        specular: 0.3, // walls originally had 0.0
+        reflective: reflective,
         ..Material::default()
     }
 }
 
-fn random_wall_material() -> Material {
-    let reflective: bool = rand::random::<f64>() <= REFLECTIVE_CHANCE;
-
-    Material {
-        pattern: random_pattern(reflective),
-        diffuse: 0.7,  // book: default
-        specular: 0.0, // book: 0.0
-        reflective: (reflective as u32) as f64,
-        ..Material::default()
-    }
-}
-
-fn add_spheres(objects: &mut Vec<Box<dyn Shape>>) {
-    let middle = Sphere {
-        transform: Matrix::translation(
-            MIDDLE_SPHERE_COORDS.0,
-            MIDDLE_SPHERE_COORDS.1,
-            MIDDLE_SPHERE_COORDS.2,
-        ) * &random_rotation(),
-        material: random_sphere_material(),
-        ..Sphere::default()
-    };
-
-    let right = Sphere {
-        transform: Matrix::translation(
-            RIGHT_SPHERE_COORDS.0,
-            RIGHT_SPHERE_COORDS.1,
-            RIGHT_SPHERE_COORDS.2,
-        ) * &Matrix::scaling(0.5, 0.5, 0.5)
-            * &random_rotation(),
-        material: random_sphere_material(),
-        ..Sphere::default()
-    };
-
-    let left = Sphere {
-        transform: Matrix::translation(
-            LEFT_SPHERE_COORDS.0,
-            LEFT_SPHERE_COORDS.1,
-            LEFT_SPHERE_COORDS.2,
-        ) * &Matrix::scaling(0.33, 0.33, 0.33)
-            * &random_rotation(),
-        material: random_sphere_material(),
-        ..Sphere::default()
-    };
-
-    objects.push(Box::new(middle));
-    objects.push(Box::new(right));
-    objects.push(Box::new(left));
-}
-
-fn add_walls(objects: &mut Vec<Box<dyn Shape>>) {
-    let floor = Plane {
-        material: random_wall_material(),
-        ..Plane::default()
-    };
-
-    let left = Plane {
-        transform: Matrix::translation(0, 0, 5)
-            * &Matrix::rotation(Axis::Y, -PI / 4.0)
-            * &Matrix::rotation(Axis::X, -PI / 2.0),
-        material: random_wall_material(),
-        ..Plane::default()
-    };
-
-    let right = Plane {
-        transform: Matrix::translation(0, 0, 5)
-            * &Matrix::rotation(Axis::Y, PI / 4.0)
-            * &Matrix::rotation(Axis::X, -PI / 2.0),
-        material: random_wall_material(),
-        ..Plane::default()
-    };
-
-    objects.push(Box::new(floor));
-    objects.push(Box::new(left));
-    objects.push(Box::new(right));
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// WORLD/CAMERA
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn prepare_world() -> World {
-    let light_source = PointLight::new((-10, 10, -10), (1, 1, 1));
+    let light_source = PointLight::new(LIGHT_POSITION, (1, 1, 1));
 
     let mut objects = vec![];
 
-    add_walls(&mut objects);
-    add_spheres(&mut objects);
+    add_objects(&mut objects);
 
     World {
         objects,
@@ -203,6 +180,10 @@ fn prepare_camera() -> Camera {
 
     camera
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// MAIN
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub fn practice() {
     let world = prepare_world();
