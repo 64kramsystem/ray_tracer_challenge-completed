b9aa4c55e253adc77e92e8811e9da0d5f810458e
Saverio Miroddi <saverio.pub2@gmail.com>
2020-11-30 21:36:26 +0100

    Implement intersections for truncated cylinders

--- library/src/space/cylinder.rs
+++ library/src/space/cylinder.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 use super::{shape, shape::private::ShapeLocal, Shape};
 use crate::{
     lang::{math::sqrt, ApproximateFloat64Ops},
@@ -13,6 +15,11 @@ pub struct Cylinder {
     pub transform: Matrix,
     #[default(Material::default())]
     pub material: Material,
+
+    #[default(f64::NEG_INFINITY)]
+    pub minimum: f64,
+    #[default(f64::INFINITY)]
+    pub maximum: f64,
 }
 
 impl ShapeLocal for Cylinder {
@@ -40,10 +47,32 @@ impl ShapeLocal for Cylinder {
         if disc < 0.0 {
             (None, None)
         } else {
-            let t0 = (-b - sqrt(disc)) / (2.0 * a);
-            let t1 = (-b + sqrt(disc)) / (2.0 * a);
+            let mut t0 = (-b - sqrt(disc)) / (2.0 * a);
+            let mut t1 = (-b + sqrt(disc)) / (2.0 * a);
 
-            (Some(t0), Some(t1))
+            if t0 > t1 {
+                mem::swap(&mut t0, &mut t1);
+            }
+
+            let mut intersections = (None, None);
+
+            let y0 = transformed_ray.origin.y + t0 * transformed_ray.direction.y;
+
+            if self.minimum < y0 && y0 < self.maximum {
+                intersections.0 = Some(t0);
+            }
+
+            let y1 = transformed_ray.origin.y + t1 * transformed_ray.direction.y;
+
+            if self.minimum < y1 && y1 < self.maximum {
+                if intersections.0.is_none() {
+                    intersections.0 = Some(t1);
+                } else {
+                    intersections.1 = Some(t1);
+                }
+            }
+
+            intersections
         }
     }
 }
--- library/src/space/cylinder_test.rs
+++ library/src/space/cylinder_test.rs
@@ -7,7 +7,8 @@ demonstrate! {
         use crate::space::{*, shape::private::ShapeLocal};
 
         before {
-            let cylinder = Cylinder::default();
+            #[allow(unused_mut)]
+            let mut cylinder = Cylinder::default();
         }
 
         it "A ray misses a cylinder" {
@@ -61,6 +62,47 @@ demonstrate! {
                 let expected_normal = Tuple::vector(*nx, *ny, *nz);
 
                 assert_eq!(cylinder.local_normal(&point), expected_normal);
+            }
+        }
+
+        it "The default minimum and maximum for a cylinder" {
+            assert_eq!(cylinder.minimum, f64::NEG_INFINITY);
+            assert_eq!(cylinder.maximum, f64::INFINITY);
+        }
+
+        it "Intersecting a constrained cylinder" {
+            cylinder.minimum = 1.0;
+            cylinder.maximum = 2.0;
+
+            let examples = [
+                // origin      direction    count
+                ((0, 1.5,  0), (0.1, 1, 0), 0), // diagonal ray from inside
+                ((0, 3.0, -5), (0.0, 0, 1), 0), // perpendicular to the y axis
+                ((0, 0.0, -5), (0.0, 0, 1), 0), // perpendicular to the y axis
+                ((0, 2.0, -5), (0.0, 0, 1), 0), // edge case: doesn't touch the max
+                ((0, 1.0, -5), (0.0, 0, 1), 0), // edge case: doesn't touch the min
+                ((0, 1.5, -2), (0.0, 0, 1), 2), // perpendicular through mid-cylinder
+            ];
+
+            for ((ox, oy, oz), (dx, dy, dz), count) in examples.iter() {
+                let origin    = Tuple::point(*ox, *oy, *oz);
+                let direction = Tuple::vector(*dx, *dy, *dz).normalize();
+                let ray = Ray { origin, direction };
+
+                let intersections = cylinder.local_intersections(&ray);
+
+                match count {
+                    2 => {
+                        assert!(intersections.0.is_some());
+                        assert!(intersections.1.is_some());
+                    },
+                    0 => {
+                        assert_eq!(intersections, (None, None));
+                    },
+                    _ => {
+                        unreachable!();
+                    }
+                };
             }
         }
     }
